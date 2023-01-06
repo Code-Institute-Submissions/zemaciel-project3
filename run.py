359565b33cefa5b307fb34aebd0bafef2561d049
zemaciel <joemacbr@gmail.com>
2023-01-06 17:18:13 +0000

    Add first for loop to check if inputed word match the random selected code

--- run.py
+++ run.py
@@ -1,19 +1,61 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-airport_list = ["DUB", "SNN", "NOC", "KIR", "ORC", "BFS"]
+airport_list = ['DUB', 'SNN', 'NOC', 'KIR']
 
 import random
 
-chosen_airport = random.choice(airport_list)
-print(f"The AirportCode Selected is {chosen_airport}")
+airport = random.choice(airport_list)
+print(f'The AirportCode Selected is {airport}')
 
+position = 0
+end_of_game = False
+
+#display = ['⬜️', '⬜️', '⬜️',]
+display = []
 guess = input("Type a 3-letter code and press enter:\n").upper()
-
-for letter in chosen_airport:
-    if letter == guess:
-        print("🟢")
+for letter in guess:
+    if letter == airport[position]:
+        display += '🟢'
+    elif letter in airport:
+        display += '🟡'
     else:
-        print("🔴")
+        display += '⬜️'
+    position += 1
+guess = guess.split()
+print(f"You guessed:\n {guess}")
+print(display)
+
+
+
+# while not end_of_game:
+
+
+# for position in range(0,2):
+#     letter = airport[position]
+#     # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+#     if letter == guess:
+#         display[position] = letter
+
+# print(display)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
